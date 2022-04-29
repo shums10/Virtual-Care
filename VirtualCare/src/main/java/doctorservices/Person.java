06160aa3df153ac65a7bc07edd500ebe9b3e8361
Akshay <barne.a@northeastern.edu>
2022-04-28 21:00:10 -0500

    Added doctor services

---
+++ VirtualCare/src/main/java/doctorservices/Person.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package doctorservices;
+
+import ngoservices.*;
+
+/**
+ *
+ * @author Akshay Barne
+ */
+public class Person {
+    String name;
+    String age;
+    House house;
+
+    public Person() {
+    }
+
+    public Person(String name, String age, House house) {
+        this.name = name;
+        this.age = age;
+        this.house = house;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public House getHouse() {
+        return house;
+    }
+
+    public void setHouse(House house) {
+        this.house = house;
+    }
+    
+    
+}
